bdc59abf1f0d41d4157fcd2733df7efc587fdb8b
Christian González <cgonzalez@opennebula.systems>
2020-05-01 20:29:56 +0200

    M #: Add testing environment

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,39 @@
 #![no_std] // Don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
+#![feature(custom_test_frameworks)]
+#![test_runner(rust_os::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
+use rust_os::println;
 
-mod vga_buffer;
 
-///This function is called on panic.
+#[cfg(not(test))]
 #[panic_handler]
+///This function is called on panic.
 fn panic(_info: &PanicInfo) -> ! {
 	println!("{}", _info);
 	loop {}
 }
 
+///This function is called on panic during testing
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+	rust_os::test_panic_handler(info)
+}
+
+///////////////////////////////////////////////////////////////////////////////
+/// Entry point
+///////////////////////////////////////////////////////////////////////////////
 #[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
 	// This function is the entry point, since the linker looks for a fucntion
 	// named `_start` by default
-
 	println!("Hello World{}", "!");
-	panic!("Some panic");
+
+	#[cfg(test)]
+	test_main();
+
 	loop{}
 }
--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -70,7 +70,7 @@ impl Writer {
 				if self.column_position >= BUFFER_WIDTH {
 					self.new_line();
 				}
-				
+
 				let row = BUFFER_HEIGHT - 1;
 				let col = self.column_position;
 
@@ -118,7 +118,6 @@ impl Writer {
 			self.buffer.chars[row][col].write(blank);
 		}
 	}
-	
 }
 
 impl fmt::Write for Writer {
@@ -143,4 +142,38 @@ macro_rules! println {
 pub fn _print(args: fmt::Arguments) {
     use core::fmt::Write;
     WRITER.lock().write_fmt(args).unwrap();
+}
+
+#[cfg(test)]
+use crate::{serial_print, serial_println};
+
+#[test_case]
+fn test_println_simple() {
+	serial_print!("test_println... ");
+	println!("test_println_simple output");
+	serial_println!("[ok]");
+}
+
+#[test_case]
+fn test_println_many() {
+	serial_print!("test_println_many... ");
+	for _ in 0..200 {
+		println!("test_println_many output");
+	}
+	serial_println!("[ok]");
+}
+
+#[test_case]
+fn test_println_output() {
+	serial_print!("test_println_output... ");
+
+	let s = "Some test string that fits on a single line";
+	println!("{}", s);
+
+	for (i, c) in s.chars().enumerate() {
+		let screen_char = WRITER.lock().buffer.chars[BUFFER_HEIGHT - 2][i].read();
+		assert_eq!(char::from(screen_char.ascii_character), c);
+	}
+
+	serial_println!("[ok]");
 }
