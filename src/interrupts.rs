ad565473e40875c82bf9a6e00fe9b50b4842cf0b
Christian González <ggmchristian@gmail.com>
2020-05-24 00:15:39 +0200

    Add page fault handler

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -1,4 +1,5 @@
 use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
+use x86_64::structures::idt::PageFaultErrorCode;
 use lazy_static::lazy_static;
 use pic8259_simple::ChainedPics;
 use spin;
@@ -6,6 +7,7 @@ use spin;
 use crate::println;
 use crate::gdt;
 use crate::print;
+use crate::hlt_loop;
 
 pub const PIC_1_OFFSET: u8 = 32;
 pub const PIC_2_OFFSET: u8 = PIC_1_OFFSET + 8;
@@ -37,6 +39,9 @@ lazy_static! {
         // Register Keyboard Interrupt handler
         idt[InterrupIndex::Keyboard.as_usize()]
             .set_handler_fn(keyboard_interrupt_handler);
+
+		// Register Page Fault handler
+		idt.page_fault.set_handler_fn(page_fault_handler);
 
         idt
     };
@@ -117,6 +122,20 @@ extern "x86-interrupt" fn keyboard_interrupt_handler(
         PICS.lock()
             .notify_end_of_interrupt(InterrupIndex::Keyboard.as_u8());
     }
+}
+
+// Page Fault handler
+extern "x86-interrupt" fn page_fault_handler(
+	stack_frame: &mut InterruptStackFrame,
+	error_code: PageFaultErrorCode,
+) {
+	use x86_64::registers::control::Cr2;
+
+	println!("EXCEPTION: PAGE FAULT");
+	println!("Accessed Address: {:?}", Cr2::read());
+	println!("Error code: {:?}", error_code);
+	println!("{:?}", stack_frame);
+	hlt_loop();
 }
 
 #[cfg(test)]
