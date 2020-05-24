6dc96059ef4a64f9b5397acb1320ca7bc46a7004
Christian González <ggmchristian@gmail.com>
2020-05-24 19:54:02 +0200

    Add memory mapper and allocator

--- src/main.rs
+++ src/main.rs
@@ -32,34 +32,26 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
 	// This function is the entry point, since the linker looks for a fucntion
 	// named `_start` by default
 
-	use rust_os::memory::active_level_4_table;
-	use x86_64::VirtAddr;
+	use rust_os::memory;
+	use rust_os::memory::BootInfoFrameAllocator;;
+	use x86_64::{structures::paging::Page, VirtAddr};
 
 	println!("Hello World{}", "!");
 
 	rust_os::init();
 
 	let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
-	let l4_table = unsafe { active_level_4_table(phys_mem_offset) };
 
-	for (i, entry) in l4_table.iter().enumerate() {
-		if !entry.is_unused() {
-			println!("L4 Entry {}: {:?}", i, entry);
+	let mut mapper = unsafe { memory::init(phys_mem_offset) };
+	let mut frame_allocator = unsafe {
+		BootInfoFrameAllocator::init(&boot_info.memory_map)
+	};
 
-			use x86_64::structures::paging::PageTable;
+	let page = Page::containing_address(VirtAddr::new(0xdeadbeaf000));
+	memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
 
-			let phys = entry.frame().unwrap().start_address();
-			let virt = phys.as_u64() + boot_info.physical_memory_offset;
-			let ptr = VirtAddr::new(virt).as_mut_ptr();
-			let l3_table: &PageTable = unsafe { &*ptr };
-
-			for (i, entry) in l3_table.iter().enumerate() {
-				if !entry.is_unused() {
-					println!("	L3 Entry {}: {:?}", i, entry);
-				}
-			}
-		}
-	}
+	let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
+	unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e)};
 
 	#[cfg(test)]
 	test_main();
